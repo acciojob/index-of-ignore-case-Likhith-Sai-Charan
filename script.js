e7ac03f51f9e3a2efc4886f34926a2d4923d6919
Likhith Sai Charan Jupudi <143252814+Likhith-Sai-Charan@users.noreply.github.com>
2025-03-17 19:15:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	const ls1=s1.toLowerCase();
+	const ls2=s2.toLowerCase();
+
+	return ls1.indexOf(ls2);
 }
 
+console.log(indexOfIgnoreCase("Hello World", "world"))
+
 // Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+///const s1 = prompt("Enter s1:");
+//const s2 = prompt("Enter s2:");
+//alert(indexOfIgnoreCase(s1, s2));
